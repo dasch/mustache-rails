221f19151adf8b4fe42a6fda22374a98390778ac
Joshua Peek <josh@joshpeek.com>
2012-06-05 15:21:29 -0500

    Test context template_name

---
+++ test/fixtures/views/template_name.rb
@@ -0,0 +1,2 @@
+class TemplateName < ActionView::Mustache
+end
--- test/test_context.rb
+++ test/test_context.rb
@@ -1,6 +1,10 @@
 require 'mustache_test'
 
 class TestContext < MustacheTest
+  def test_template_name
+    assert_equal "template_name", @view.render(:template => "template_name")
+  end
+
   def test_view_class_method
     assert_equal "Hello, World!", @view.render(:template => "hello")
   end
