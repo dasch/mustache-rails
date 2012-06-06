ad48a9c471bd30a5bc22b65417148b0e23908783
Joshua Peek <josh@joshpeek.com>
2012-06-05 21:46:13 -0500

    Doc mustache helper

--- lib/action_view/helpers/mustache_helper.rb
+++ lib/action_view/helpers/mustache_helper.rb
@@ -6,13 +6,35 @@ require 'action_view/mustache'
 
 module ActionView
   module Helpers
+    # Public: Standard helper mixed into ActionView context for
+    # constructing the Mustache View.
     module MustacheHelper
       extend ActiveSupport::Concern
 
       included do
+        # PID: Storing our view namespace settings on a class variable
+        # is kinda nasty. It'd better to avoid the global and always
+        # use our application's config object.
         cattr_accessor :mustache_view_namespace
       end
 
+      # Public: Get Mustache View Class for template.
+      #
+      # Choose template class by camalizing the template path name and
+      # prefixing our view namespace.
+      #
+      # The class must be a subclass of ActionView::Mustache
+      # otherwise a TypeError will be raised.
+      #
+      # If the template is "anonymous" and has no path, which is the
+      # case for render(:inline), the default ActionView::Mustache
+      # base class will be returned.
+      #
+      # Examples
+      #
+      #     "blog/show" => Blog::Show
+      #
+      # Returns Class or raises a TypeError.
       def mustache_view_class
         return ActionView::Mustache unless @virtual_path
         klass = "#{mustache_view_namespace}/#{@virtual_path}".camelize.constantize
@@ -22,6 +44,13 @@ module ActionView
         klass
       end
 
+      # Public: Get or initialize Mustache View.
+      #
+      # The view object is cached so partial renders will reuse the
+      # same instance.
+      #
+      # Returns a Mustache View which is a subclass of
+      # ActionView::Mustache.
       def mustache_view
         @_mustache_view ||= mustache_view_class.new(self)
       end
