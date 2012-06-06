20a2e706a511d23a3788255ecc629dfc7076d8b2
Joshua Peek <josh@joshpeek.com>
2012-06-05 22:10:39 -0500

    Doc mustache class

--- lib/action_view/mustache.rb
+++ lib/action_view/mustache.rb
@@ -2,17 +2,43 @@ require 'action_view'
 require 'mustache'
 
 module ActionView
+  # Public: Mustache View base class.
+  #
+  # All Mustache views MUST inherit from this class.
+  #
+  # Examples
+  #
+  #     module Layouts
+  #       class Application < ActionView::Mustache; end
+  #     end
+  #
   class Mustache < ::Mustache
+    # Internal: Override Mustache's default Context class
     class Context < ::Mustache::Context
-      undef_method :partial, :escapeHTML
+      # Partials are no longer routed through Mustache but through
+      # ActionView's own render method.
+      undef_method :partial
+
+      # Escape helper isn't used, its all handled by Rails' SafeBuffer
+      # auto escaping.
+      undef_method :escapeHTML
     end
 
+    # Internal: Initializes Mustache View.
+    #
+    # Initialization is handled by MustacheHelper#mustache_view.
+    #
+    # view - ActionView::Base instance
+    #
+    # Returns ActionView::Mustache instance.
     def initialize(view)
-      # Reference to original AV context.
+      # Reference to original ActionView context.
       @_view = view
 
+      # Grab template path from view
       self.template_name = view.instance_variable_get(:@virtual_path)
 
+      # If view has an associated controller
       if controller = view.controller
         # Copy controller ivars into our view
         controller.view_assigns.each do |name, value|
@@ -24,19 +50,27 @@ module ActionView
       context[:yield] = lambda { content_for :layout }
     end
 
-    # Use AV's render instead of Mustache
+    # Remove Mustache's render method so ActionView's render can be
+    # delegated to.
     undef_method :render
 
+    # Public: Get view context.
+    #
+    # Returns ActionView::Mustache::Context instance.
     def context
       @context ||= Context.new(self)
     end
 
-    # Forwards methods to original Rails view context
+    # Public: Forwards methods to original Rails view context
+    #
+    # Returns an Object.
     def method_missing(*args, &block)
       @_view.send(*args, &block)
     end
 
-    # Checks if method exists in Rails view context
+    # Public: Checks if method exists in Rails view context.
+    #
+    # Returns Boolean.
     def respond_to?(method, include_private = false)
       super || @_view.respond_to?(method, include_private)
     end
